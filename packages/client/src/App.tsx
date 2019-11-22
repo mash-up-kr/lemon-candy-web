3c9c426a2778b8c3ddf7b7118aced6ebdae05bd7
Donggeun Lee <eastroots92@gmail.com>
2019-11-22 23:20:17 +0900

    Add esLint

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      안녕하세요
-    </div>
-  );
-};
+const App: React.FC = () => (
+  <div className="App">
+    안녕하세요
+  </div>
+);
 
 export default App;
